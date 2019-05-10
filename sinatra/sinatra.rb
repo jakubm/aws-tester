f7d833f40343f751ba63afc2321c5af2c9dbcdcf
Jakub Mišina <jakub.misina@gmail.com>
2019-05-10 19:56:05 +0200

    psql included

--- sinatra/sinatra.rb
+++ sinatra/sinatra.rb
@@ -1,14 +1,36 @@
 require 'sinatra'
 require 'puma'
 require 'httparty'
+require 'pg'
 
 counterhost = ENV['COUNTERHOST']
 counterport = ENV['COUNTERPORT']
 
+pghost = ENV['PGHOST']
+pgdatabase = ENV['PGDATABASE']
+pguser = ENV['PGUSER']
+pgpassword = ENV['PGPASSWORD']
+
 set :port, ENV['PORT']
+
+conn = PG.connect(host: pghost, dbname: pgdatabase, user: pguser, password: pgpassword)
+conn.exec("create table if not exists test_data (id bigserial, t text)")
+conn.close
 
 get '/' do
   counter = HTTParty.get("http://#{counterhost}:#{counterport}/").body
-  output = "<pre>counter: #{counter}"
+  output = "<pre>counter: #{counter}</pre><pre>"
+
+  conn = PG.connect(host: pghost, dbname: pgdatabase, user: pguser, password: pgpassword)
+  conn.exec("insert into test_data (t) values ('Hello, World')")
+  counter = 0
+  conn.exec("select id, t from test_data order by id desc limit 25") do |result|
+    result.each do |row|
+      counter += 1
+      output += "#{counter}  %-7d %s\n" % row.values_at('id', 't')
+    end
+  end
+  conn.close
+
   output + "</pre>"
 end
