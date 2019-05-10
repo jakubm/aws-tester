f1d7706cdbf9d78c7274fa2e6c37b9f02607944b
Jakub Mišina <jakub.misina@gmail.com>
2019-05-10 19:41:27 +0200

    fix 2

--- sinatra/sinatra.rb
+++ sinatra/sinatra.rb
@@ -8,7 +8,7 @@ counterport = ENV['COUNTERPORT']
 set :port, ENV['PORT']
 
 get '/' do
-  counter = HTTParty.get("http://#{counterhost}:#{counterport}/").body
+  # counter = HTTParty.get("http://#{counterhost}:#{counterport}/").body
   counter |= 999
   output = "<pre>counter: #{counter}"
   output + "</pre>"
