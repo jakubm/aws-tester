602e321e56dcd2226601dec91b66753a7f01bd5f
Jakub Mišina <jakub.misina@gmail.com>
2019-05-10 21:20:49 +0200

    removal or redis

--- counter/counter.rb
+++ counter/counter.rb
@@ -1,14 +1,11 @@
 require 'sinatra'
 require 'puma'
-require 'redis'
 
 set :port, ENV['PORT']
-redishost = ENV['REDISHOST']
 
 counter = 0
 
 get '/' do
-  redis = Redis.new(host: redishost)
-  counter = redis.incr('counter')
+  counter += 1
   counter.to_s
 end
